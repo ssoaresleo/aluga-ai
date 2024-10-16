f330717348bf26040ed9d97201ec7a3a026d88ae
Leonardo Henrique <leonardohenriquesoarasalvez784@gmail.com>
2024-10-15 22:48:34 -0300

    feat: add filter

--- src/pages/_layout/app.tsx
+++ src/pages/_layout/app.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../../components/ui/header";
+import { StayFilter } from "./components/ stay-filter";
 
 export const AppLayout = () => {
   return (
     <div className="flex min-h-screen flex-col py-5 container mx-auto">
       <Header />
+
+      <StayFilter />
 
       <div className="flex flex-1 flex-col gap-12 mt-10">
         <Outlet />
--- src/pages/app/home/index.tsx
+++ src/pages/app/home/index.tsx
@@ -1,3 +1,5 @@
+import { Fragment } from "react/jsx-runtime";
+
 export const Home = () => {
-  return <></>;
+  return <Fragment></Fragment>;
 };
